3488381f15843b409a9c3817e38c49a009d5a9ee
Den <deb@honor.com>
2023-09-15 12:52:09 -0400

    move slider.js to js/modules

--- js/about.js
+++ js/about.js
@@ -1,16 +1,14 @@
-var id = null;
-var deg = 0;
-var limit = 0;
+let id = null;
+let deg = 0;
+let limit = 0;
 
-var facts = ["1 Fact <br> У медуз нет мозгов и кровеносных сосудов.", "2 Fact <br>Китайский язык является самым популярным в мире.", "3 Fact <br>Правое лёгкое человека вмещает больше воздуха, чем левое."];
+let facts = ["1 Fact <br> У медуз нет мозгов и кровеносных сосудов.", "2 Fact <br>Китайский язык является самым популярным в мире.", "3 Fact <br>Правое лёгкое человека вмещает больше воздуха, чем левое."];
 
-var fact_id = 0;
+let fact_id = 0;
 
-// console.log("Start");
 
 function setFact(){
     const el = document.getElementById("fact");
-    // el.textContent = facts[fact_id];
     el.innerHTML = facts[fact_id];
 
     fact_id += 1;
@@ -18,19 +16,19 @@ function setFact(){
 }
 
 function getFact() {
-    var elem = document.getElementById("wheel");
+    let elem = document.getElementById("wheel");
     clearInterval(id);
     limit+=800;
     id = setInterval(frame, 10);
+
     function frame() {
       if (deg >= limit) {
         clearInterval(id);
         setFact();
       } else {
         deg+=3;
-        elem.style.transform = "rotate("+deg%360+"deg)";
+        elem.style.transform = "rotate(" + deg % 360 + "deg)";
       }
     }
   }
 
-// getFact();
