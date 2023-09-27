0f4ae2efa976e28d3743bc5fcc0f068f3c90d9d7
Den <deb@honor.com>
2023-09-27 03:58:58 -0400

    refactor 'models.py' add func to create test db adn view all necessary data

--- PriceWatcher/settings.py
+++ PriceWatcher/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import environ
 
+from sqlalchemy import create_engine
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
--- Web/models.py
+++ Web/models.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
-from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey, select, Boolean
+from sqlalchemy import create_engine, Column, Integer, Float, String, Date, ForeignKey, select, Boolean
 from sqlalchemy.orm import declarative_base, sessionmaker, Session, relationship, mapped_column, Mapped, relationship
 
 from typing import List
 from datetime import datetime, date
 
-engine = create_engine("sqlite:///test.db")#, echo=True)
-conn = engine.connect() 
+import random
 
-# Create DB
+
+
+ENGINE = create_engine("sqlite:///price_watcher.db") #, echo=True)
+
 
 Base = declarative_base()
 
@@ -101,154 +103,116 @@ class Comment(Base):
     product: Mapped["Product"] = relationship()
     user: Mapped["User"] = relationship()
 
-# class Parent(Base):
-#     __tablename__ = "parent_table"
 
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     children: Mapped[List["Child"]] = relationship()
-#     name: Mapped[String] = mapped_column(String)
+class Currency(Base):
+    __tablename__ = "currency"
 
+    id: Mapped[int] = mapped_column(Integer, primary_key = True)
+    name: Mapped[String] = mapped_column(String, unique=True, nullable=False)
 
-# class Child(Base):
-#     __tablename__ = "child_table"
+    exchange_rates: Mapped[List["ExchangeRates"]] = relationship()
 
-#     id: Mapped[int] = mapped_column(primary_key=True)
-#     parent_id: Mapped[int] = mapped_column(ForeignKey("parent_table.id"))
-#     price: Mapped[int] = mapped_column(Integer)
+class ExchangeRates(Base):
+    __tablename__ = "exchange_rate"
 
+    date: Mapped[Date] = mapped_column(Date, primary_key=True)
+    currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"), primary_key=True)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
 
-
-Base.metadata.create_all(bind=engine)
-
-
-
-with Session(autoflush=True, bind=engine) as db:
-    # p1 = Product(id=1, name='Hi tech')
-    # p2 = Product(id=2, name='Hi Chech')
-
-    # s1 = Shop(id=1, name='DNS')
-    # s2 = Shop(id=2, name='Citilink')
-    # s3 = Shop(id=3, name='Regard')
-
-    # u1 = Url(id=1,product_id=p1.id, shop_id=s1.id, url='https://product1')
-    # u2 = Url(id=2,product_id=p1.id, shop_id=s2.id, url='https://product1&shop=2')
-    # u3 = Url(id=3,product_id=p1.id, shop_id=s3.id, url='https://product1&shop=3')
-
-    # u4 = Url(id=4,product_id=p2.id, shop_id=s1.id, url='https://product1001')
-
-    # pp1 = ProductPrice(url_id=u1.id, date=datetime.now(), price=100)
-    # pp2 = ProductPrice(url_id=u2.id, date=datetime.now(), price=110)
-    # pp3 = ProductPrice(url_id=u3.id, date=datetime.now(), price=200)
-
-    # pp5 = ProductPrice(url_id=1, date=date(2022, 12, 1), price=80)
-    # pp6 = ProductPrice(url_id=2, date=date(2022, 12, 1), price=40)
-    # pp7 = ProductPrice(url_id=3, date=date(2022, 12, 1), price=65)
-
-    # pp4 = ProductPrice(url_id=u4.id, date=datetime.now(), price=80)
-
-    # db.add(p1)
-    # db.add(p2)
-
-    # db.add(s1)
-    # db.add(s2)
-    # db.add(s3)
-
-    # db.add(u1)
-    # db.add(u2)
-    # db.add(u3)
-    # db.add(u4)
- 
-    # db.add(pp1)
-    # db.add(pp2)
-    # db.add(pp3)
-    # db.add(pp4)
-    # db.add(pp5)
-    # db.add(pp6)
-    # db.add(pp7)
-
-    # db.commit()
-
-
-    # # urls = db.query(Url).all()
-
-    # # for i in urls:
-    # #     print(i.product.name, i.shop.name, i.url)
-    # #     for j in i.price:
-    # #         print(f"Price: {j.price}")
-
-
-    # r1 = Role(id=1, name="Admin")
-    # r2 = Role(id=2, name="User")
-
-    # user_1 = User(email="reer@gmail.com", sault="yue", password="1230r002", role_id=1, firstname="Bob", lastname="Rore")
-    # user_2 = User(email="mic@gmail.com", sault="ay3", password="admin123", role_id=2, firstname="BIB", lastname="Case")
-
-    # db.add(r1)
-    # db.add(r2)
-    # db.add(user_1)
-    # db.add(user_2)
-    # db.commit()
-
-    # user = db.query(User).first()
-    # print(user.firstname, user.role.name, user.active)
-
-    # # ToDo Test comments in rable 'Product'
-    # c1 = Comment( product_id=1, user_id=1, comment="Good product", stars=5)
-    # c2 = Comment(product_id=1, user_id=2, comment="do not shure it is good", stars=2)
-
-    # db.add(c1)
-    # db.add(c2)
-    # db.commit()
+    currencies: Mapped["Currency"] = relationship()
 
 
 
+conn = ENGINE.connect() 
 
-    products = db.query(Product).all()
 
-    for i in products:
-        print(i.name)
-        for j in i.urls:
-            price = sorted(j.prices, key=lambda price: price.date, reverse=True)
-            print(f'\t{j.shop.name} {j.url} {price[0].price}')
+def add_test_data():
+    products = ["Монитор Huawei MateView SE SSN-24 23.8", "Кофеварка KitFort КТ-715", 
+                "Водоочиститель Аквафор Кристалл", "Кулер AEL LD-AEL-28c, напольный, электронный",
+                 "Кондиционер Сплит-система CENTEK CT-65E12 настенная", "Швейная машина Janome Excellent Stitch 100",
+                  "Сушилка для рук G-TEQ 8860 PS"]
+    shops = ["Citilink", "Positronica", "Video-shoper", "Regard"]
+
+    roles = ["Admin", "User"]
+    users = [["reer@gmail.com", 1, "Bob", "Rore"],
+        ["lib@gmail.com", 1, "Tom", "Rom"],
+        ["more@mail.ru", 1, "Lander", "Mos"], 
+        ["qoe@gmail.com", 1, "Cress", "Zoop"],
+        ["ads@yandex.ru", 0, "Adiv", "Urs"]]
+    currencies = ["Доллар", "Евро", "Така", "Франк"]
+
+    comments = ["The best product", "Not bad", "It's horrible", "Woow!", 'I can recommend this product']
+
+    with Session(autoflush=True, bind=ENGINE) as db:
+
+        for i, name in enumerate(roles):
+            db.add(Role(id=i, name=name))
+
+        for i, values in enumerate(users):
+            db.add(User(email=values[0], sault="123", password="test_pass", role_id=values[1], firstname=values[2], lastname=values[3]))
+
+
+        for i, name in enumerate(products):
+            db.add(Product(id=i, name=name))
+            for j in range(random.randint(1, 8)):
+                db.add(Comment(product_id=i, user_id=random.randint(1, len(users)), comment=random.choice(comments), stars=random.randint(0, 5)))
+
+        for i, name in enumerate(shops):
+            db.add(Shop(id=i, name=name))
+
+        id = 0
+        for i, value in enumerate(products):
+            for j, name in enumerate(shops):
+                db.add(Url(id=id, product_id=i, shop_id=j, url=f"https://{name}/{value}/"))
+                for d in range(10):
+                    db.add(ProductPrice(url_id=id, date=date(2022, 12, 1+d), price=random.randint(2500, 55000)))
+
+                id += 1
+
         
-        print("Comments:")
-        for j in i.comments:
-            print(f"\t{j.user.firstname}: {j.comment}| {j.stars}")
+
+        for i, name in enumerate(currencies):
+            db.add(Currency(id=i, name=name))
+            for j in range(10):
+                db.add(ExchangeRates(date=date(2022, 12, 1+j), currency_id=i, price=random.randint(2000, 20000)/100))
+        
+
+        db.commit()
+
+def selection_of_db():
+    with Session(autoflush=True, bind=ENGINE) as db:
+
+        print('Товары:')
+        products = db.query(Product).all()
+        for i in products:
+            print(i.name)
+            for j in i.urls:
+                price = sorted(j.prices, key=lambda price: price.date, reverse=True)
+                print(f'\t{j.shop.name} {j.url}\t{price[0].price}')
+            
+            print("Comments:")
+            for j in i.comments:
+                print(f"\t{j.user.firstname}: {j.comment}| {j.stars}")
+
+            print("\n\n")
 
 
-    print('\n\n\nCheck')
-    prices = db.query(ProductPrice).order_by(ProductPrice.date.desc())
-    for i in prices:
-        print(i.url.product.name, i.date, i.price)
+        print("\nВалюты")
+        u = db.query(Currency).all()
+        for i in u:
+            print(i.name)
 
+            prices = sorted(i.exchange_rates, key=lambda x: x.date, reverse=True)
 
-    # for i in table_join:
-    #     print(*i)
+            for j in prices:
+                print(f"\t{j.date} | {j.price}")
 
-#     # parent_1 = Parent(id=44, name = 'Bob')
-#     # parent_2 = Parent(id=43, name = 'Tom')
-
-#     # # db.add(parent_1)
-#     # # db.add(parent_2)
-
-#     # child_1 = Child(parent_id=parent_1.id, price=2000)
-#     # child_2 = Child(parent_id=parent_2.id, price=25)
-
-    
-    
-#     # db.add(child_1)
-#     # db.add(child_2)
-  
-
-#     # db.commit()
-
-
-#     parent = db.query(Parent).filter(Parent.name == "Tom", Parent.id > 3).first()
-#     print(parent.id)
-#     for i in parent.children:
-#         print(i.id, i.price)
+            print("\n\n")
 
 
 
-   
+if __name__ == "__main__":
+    Base.metadata.create_all(bind=ENGINE)
 
+    # add_test_data()
+    selection_of_db()
